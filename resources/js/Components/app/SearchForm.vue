604ae5d30cb3db3f7d1ead44c5b5f1a163464e7e
FiatFour <fiatonetwo3@gmail.com>
2024-01-02 04:06:54 +0000

    first commit

---
+++ resources/js/Components/app/SearchForm.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import TextInput from "@/Components/TextInput.vue";
+import {useForm} from "@inertiajs/vue3";
+
+const form = useForm({
+    search: ''
+})
+</script>
+
+<template>
+    <form class="w-[600px] h-[80px] flex items-center">
+        <TextInput type="text"
+                   class="block w-full mr-2"
+                   v-model="form.search"
+                   autocomplate
+                   placeholder="Search for files and foders"/>
+    </form>
+</template>
+
+<style scoped>
+
+</style>
